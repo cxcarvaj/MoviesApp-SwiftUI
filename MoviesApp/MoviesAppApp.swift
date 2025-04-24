54e48ffe13c741be25f27b3a2c292e36b305835c
cxcarvaj <cxcarvaj@gmail.com>
2025-04-24 16:21:38 -0500

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Carlos Xavier Carvajal Villegas on 24/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
